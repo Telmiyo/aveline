b232033f2a1c64cdafb8b69e56f9757307aa1643
Telmiyo <iamtelmo@proton.me>
2024-12-15 20:16:16 +0100

    refactor(dashboard): rename views dir to pages

--- desktop-app/src/renderer/router/routes.tsx
+++ desktop-app/src/renderer/router/routes.tsx
@@ -3,9 +3,9 @@ import { Navigate } from 'react-router-dom';
 import Reader from '@aveline/src/renderer/modules/reader';
 
 const Dashboard = lazy(() => import('@dashboard/index'));
-const Home = lazy(() => import('@dashboard/views/home/home'));
-const Bookmarks = lazy(() => import('@dashboard/views/bookmarks/bookmarks'));
-const Settings = lazy(() => import('@dashboard/views/settings/settings'));
+const Home = lazy(() => import('@dashboard/pages/home/home'));
+const Bookmarks = lazy(() => import('@dashboard/pages/bookmarks/bookmarks'));
+const Settings = lazy(() => import('@dashboard/pages/settings/settings'));
 
 const routes = [
   {
